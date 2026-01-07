3c725ed34844a4c2935725406399417172a77257
raikar8 <43542167+raikar8@users.noreply.github.com>
2026-01-07 17:52:00 +0530

    Re-Mix style Time-Stamp routing for Mixture of Experts

--- speechbrain/integrations/models/__init__.py
+++ speechbrain/integrations/models/__init__.py
@@ -1,3 +1,8 @@
 """
 Package with models from stand-alone repos (i.e. not SpeechBrain or Huggingface).
 """
+
+from speechbrain.integrations.models.sgmse_plus import ScoreModel
+from speechbrain.integrations.models.sgmse_moe import ScoreModelMoE
+
+__all__ = ["ScoreModel", "ScoreModelMoE"]
